a79dfdb18cfe27b4b8ef599f7412253b970da50d
banagaurav <gauravbana840@gmail.com>
2024-09-25 11:16:11 +0545

    add Strings example with String functions

---
+++ php/Strings.php
@@ -0,0 +1,20 @@
+<?php
+echo ("Hello World");
+?>
+
+<?php
+echo ("We\'ll do it."); // output We'll do it.
+
+//string functions
+echo strlen("Hello World"); // output 11
+
+echo str_word_count("Hello World"); // output 2
+
+echo strrev("Hello World!"); // output !dlroW olleH
+
+echo strpos("Hello World", "World"); // output 6
+
+//syntax : ("To be replace","by which to replace","preset string")
+echo str_replace("World", "Bana", "Hello World"); //outputs Hello Bana
+
+?>
